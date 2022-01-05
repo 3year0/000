5ac9a55f58ee17607c6b6ea6b121890e4a712e82
taeJunPark <taejunp116@gmail.com>
2022-01-05 23:06:23 +0900

    array

---
+++ example/array/RUN.java
@@ -0,0 +1,17 @@
+package example.array;
+
+public class RUN {
+
+	public static void main(String[] args) {
+		array ar = new array();
+			
+			//ar.arrayPractice();
+			ar.arrayPractice2();
+			
+		
+			
+			
+		
+	}
+
+}
