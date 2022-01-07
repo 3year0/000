0facad61b0c93ba0b6485ac1224bfddd7b70d007
taeJunPark <taejunp116@gmail.com>
2022-01-07 23:42:37 +0900

    2중배열 실습

--- arrayTest1/RUN.java
+++ arrayTest1/RUN.java
@@ -5,7 +5,10 @@ public class RUN {
 	public static void main(String[] args) {
 		arraytest1 ar = new arraytest1();
 		//ar.arraytest1();
-		ar. method1();
+		//ar. method1();
+		ar.ar2 ();
+		
+		
 	}
 
 }
--- arrayTest1/arraytest1.java
+++ arrayTest1/arraytest1.java
@@ -54,4 +54,35 @@ public class arraytest1 {
 	}
 	System.out.print("프로그램을 종료합니다.");
  }
+	
+	public void ar2 () {
+		String[] arSubject = {"Java", "Python", "Android", "JSP", "HTML"};
+        int[] arScore = {95, 88, 76, 62, 55};
+        
+        Scanner sc = new Scanner(System.in);
+        
+        while (true) {
+            System.out.print("과목명을 입력하세요: ");
+            String name = sc.next();
+            
+            for(int i=0; i<arSubject.length; i++) {
+                if (arSubject[i].equals(name)) {
+                    System.out.println(arSubject[i] + "의 점수: " + arScore[i]);
+                }
+            }
+            
+            if (name.equals("그만")) {
+                break;
+            }
+        }
+    
+        sc.close();
+        
+    }
+
+
+
+	
 }
+
+
